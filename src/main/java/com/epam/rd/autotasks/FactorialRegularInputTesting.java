cb3901587b25cdbc4ef4c1466f8321b7e398d21e
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-09-25 20:42:54 +0400

    Test Factorial: Commit N3

--- src/main/java/com/epam/rd/autotasks/FactorialRegularInputTesting.java
+++ src/main/java/com/epam/rd/autotasks/FactorialRegularInputTesting.java
@@ -11,4 +11,16 @@ class FactorialRegularInputTesting {
         String factorialOfN = factorial.factorial("5");
         Assertions.assertEquals("120", factorialOfN);
     }
+
+    @Test
+    public void regularInputTest12() {
+        String factorialOfN = factorial.factorial("12");
+        Assertions.assertEquals("479001600", factorialOfN);
+    }
+
+    @Test
+    public void regularInputTest15() {
+        String factorialOfN = factorial.factorial("15");
+        Assertions.assertEquals("2004310016", factorialOfN);
+    }
 }
